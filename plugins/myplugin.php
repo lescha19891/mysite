abbb270aeacd75465482714d15996a613f9c677e
lescha19891 <74153713+lescha19891@users.noreply.github.com>
2020-11-16 02:28:57 +0300

    Add files via upload

---
+++ plugins/myplugin.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name: Мой плагин
+*/
+function error1($error) {
+	
+$f=fopen("error1.txt","a+"); 
+if  (filesize("error1.txt") != 0) {
+$text=fread($f,filesize("error1.txt"));
+	fclose($f);
+}
+	$f=fopen("error1.txt", "w");
+	$write="Error: $error ";
+	fwrite($f, $write . $text);
+	fclose($f);
+}
